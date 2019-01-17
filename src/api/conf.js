3b7657ea3e48504c0a0dbdce3e1575d4276b0e04
LiuLongTian <1750625203@qq.com>
2019-01-17 23:21:13 +0800

    no message

--- src/api/conf.js
+++ src/api/conf.js
@@ -1,2 +1,2 @@
-export const base_url = "https://pbms.onv.com.cn/api"
-// export const base_url = "http://203.195.236.254:8886/yidekuai"
+// export const base_url = "https://pbms.onv.com.cn/api"
+export const base_url = "http://203.195.236.254:8886/yidekuai"
