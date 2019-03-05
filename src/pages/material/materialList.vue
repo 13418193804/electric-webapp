30b04445e106ed03fabcb6719aed85565bbc6e80
LiuLongTian <1750625203@qq.com>
2019-03-05 16:25:29 +0800

    no message

--- src/pages/material/materialList.vue
+++ src/pages/material/materialList.vue
@@ -19,19 +19,24 @@
         </div>
         <div class="flex details-content">
             <div class="details-content-box">
-                <div class="details-content-box-list" v-for="(item,index) in listData" :key="index"
+                <div class="details-content-box-list" v-for="(item,index) in warehouse" :key="index"
                     @click="handelLook(item,index)" :class="{cur:index == active}"
-                >{{item.name}}</div>
+                >{{item.type_name}}</div>
             </div>
              <div class="details-content-box">
+
+                <div v-for="(item,index) in valueList">
                  <div class="flex details-content-box-details">
-                     <div>型号</div>
-                     <div>98766789-K</div>
+                     <div>{{item.name}}</div>
+                     <!-- <div>98766789-K</div> -->
                  </div>
                  <div class="flex details-content-box-details">
                      <div>库存:</div>
-                     <div>{{sum}} <span @click="getMinute()"><img src="../../assets/jian.png" alt=""></span> {{nowNum}} <span @click="getAdd()"><img src="../../assets/jia.png" alt=""></span></div>
+                     <div>{{item.amount}} <span @click="getMinute(item)"><img src="../../assets/jian.png" alt=""></span> {{getCount(item.id)}} <span @click="getAdd(item)"><img src="../../assets/jia.png" alt=""></span></div>
                  </div>
+                </div> 
+
+
             </div>
         </div>
         <div class="details-form">
@@ -44,7 +49,7 @@
             <div class="details-form-list">
                 申请物料：
                 <div class="details-form-list-border">
-                    98766789-K整机一台；网线10米；保护开关1个
+                    {{getPreMaterialListString()}}
                 </div>
             </div>
              <div class="details-form-list">
@@ -54,6 +59,9 @@
                 </div>
             </div>
         </div>
+
+           <md-button type="default" @click="postMaterialwarehouse">Default</md-button>
+
     </div>
   </div>
 </template>
@@ -70,7 +78,6 @@ export default {
   data() {
     return {
       active: 0,
-      sum: 11,
       nowNum: 1,
       listData: [
         { name: "机箱" },
@@ -79,11 +86,13 @@ export default {
         { name: "网线" },
         { name: "电源保护开关" }
       ],
-   
+      warehouse: [],
+      valueList: [],
+      preMaterialList: []
     };
   },
   mounted() {
-      this.getDataList()
+    this.getMaterialwarehouse();
   },
   methods: {
     leftClick() {
@@ -91,21 +100,92 @@ export default {
     },
     handelLook(item, index) {
       this.active = index;
+      console.log(item);
+      this.valueList = item.data;
     },
-    getMinute() {
-      if (this.nowNum === 1) {
-        return;
+    //实时取数量
+    getCount(id) {
+      let list = this.preMaterialList.filter((item, index) => {
+        return item.id == id;
+      });
+      if (list.length > 0) {
+        return list[0].getCount;
       } else {
-        this.nowNum = this.nowNum - 1;
+        return 0;
       }
     },
-    getAdd() {
-      if (this.nowNum === this.sum) {
-        return;
-      } else {
-        this.nowNum = this.nowNum + 1;
-      }
-    }
+    getIdx(id, callback) {
+      let idx = null;
+      new Promise(resolve => {
+        this.preMaterialList.filter((item, index) => {
+          if (item.id == id) idx = index;
+          return item.id == id;
+        });
+        resolve(idx);
+      }).then(res => {
+        callback(res);
+      });
+    },
+    getAdd(row) {
+      this.getIdx(row.id, idx => {
+        if (idx != null) {
+          let getCount = this.preMaterialList[idx].getCount || 0;
+          if (getCount < row.amount) {
+            getCount += 1;
+            this.preMaterialList[idx].getCount = getCount;
+          }
+        } else {
+          this.preMaterialList.push(Object.assign(row, { getCount: 1 }));
+        }
+        this.preMaterialList.push();
+      });
+    },
+    getMinute(row) {
+      let idx = this.getIdx(row.id, idx => {
+        if (idx != null) {
+          let getCount = this.preMaterialList[idx].getCount || 0;
+          if (getCount > 0) {
+            getCount -= 1;
+            this.preMaterialList[idx].getCount = getCount;
+            this.preMaterialList.push();
+          }
+        }
+      });
+    },
+    getPreMaterialListString() {
+    
+      let list = this.preMaterialList.filter(filter=>{
+        return (filter.getCount||0) >0
+      }).map(item => {
+        return `${item.name}${item.getCount}${item.units}`;
+      });
+ 
+      return list.join("；");
+    },
+    //取物料仓库
+    getMaterialwarehouse() {
+      let data = {
+        token: this.$store.getters.getToken
+      };
+      this.service
+        .httpRequest({
+          url: "/aapi/materialwarehouse",
+          methods: "get",
+          data: data
+        })
+        .then(res => {
+          if (res.returnStatus) {
+            this.warehouse = res.data.data.warehouse;
+          } else {
+            this.$dialog.alert({
+              content: res.msg,
+              confirmText: "确定"
+            });
+          }
+        });
+    },
+    //提交物料申请
+    postMaterialwarehouse() {}
   }
 };
 </script>
@@ -135,7 +215,7 @@ export default {
     }
   }
   &-content {
-    margin: 40 * @rpx 0 30* @rpx;
+    margin: 40 * @rpx 0 30 * @rpx;
     &-box {
       border: 1px solid #ddd;
       width: 70%;
